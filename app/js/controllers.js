3a4b31a451998b2d471a630438b6b3ae883b4373
Vasin Dmitriy <vasindima779@gmail.com>
2013-09-24 16:21:02 +0300

    intermidiate commit ( initializing angular )

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,11 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('MyCtrl_galery', ['$scope', '$routeParams', function($scope, $routeParams) {
+  	$scope.setImage = function(imageUrl) {
+		  $scope.mainImageUrl = imageUrl;
+		}
+		$scope.data = getPicture('birds', 4);
+  	// $scope.phoneId = $routeParams.phoneId;
   }]);
