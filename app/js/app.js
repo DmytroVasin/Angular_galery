448ecec8aecaaf6fc385fc52fc10883da2e71315
Vasin Dmitriy <vasindima779@gmail.com>
2013-09-26 00:33:55 +0300

    Reafactoring, removed view

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('Galery', ['ngRoute', 'Galery.controllers', 'Galery.services'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'partials/partial_galery.html', controller: 'MyCtrl_galery'});
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+angular.module('Galery', ['Galery.controllers', 'Galery.services'])
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,21 +2,9 @@
 
 /* Controllers */
 
-angular.module('Galery.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }])
-  .controller('MyCtrl_galery', ['$scope', '$routeParams', 'Photos',
-                      function(  $scope,   $routeParams,   Photos ) {
+angular.module('Galery.controllers', [])
+  .controller('GaleryCtrl', ['$scope', 'Photos',
+                    function( $scope,   Photos ) {
 
     $scope.photos = Photos.getPicture('girls',2);
-
-    // $scope.item = 'some item'
-    //$scope.setImage = function(imageUrl) {
-		//   $scope.mainImageUrl = imageUrl;
-		// }
-  	// $scope.phoneId = $routeParams.phoneId;
   }]);
