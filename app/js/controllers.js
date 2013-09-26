f1861fc48f77c289b4cbe0d934eb5a5814bf737c
Vasin Dmitriy <vasindima779@gmail.com>
2013-09-26 18:32:35 +0300

    Added Localstorage BEGINING!

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,6 @@ angular.module('Galery.controllers', [])
                     function( $scope,   Photos,   Finder ) {
 
     $scope.photos = Photos.getPicture('girls', 1);
-    $scope.ajax_loading = false;
     $scope.current_page = 1;
     $scope.isDisabled = false;
 
@@ -40,7 +39,22 @@ angular.module('Galery.controllers', [])
 
     $scope.addToFolder = function(photo){
       // console.log(photo);
-      $scope.login = Finder.add(photo)
+      // localStorage.setItem("file", photo);
+      // console.log(localStorage.getItem("todoList"));
+
+      // [
+      //  { type: 'folder', id: 1, parent: null, name: 'HOME' },
+      //  { type: 'file',   id: 2, parent: 1,    name: 'screenshot_1.jpg', src: 'http://fa...' }
+      //  { type: 'file',   id: 3, parent: 1,    name: 'screenshot_2.jpg', src: 'http://fa...' }
+      //  { type: 'folder', id: 4, parent: 1,    name: 'SEA' },
+      //  { type: 'file',   id: 5, parent: 4,    name: 'screenshot_3.jpg', src: 'http://fa...' }
+      // ]
+
+
+
+
+
+      // $scope.login = Finder.add(photo)
       // LocalStorage.set =
     };
     // $watch(Finder.active). => перерисовать папку
