c04155255babb14a479f79c298e45ef6cc0da3cf
Vasin Dmitriy <vasindima779@gmail.com>
2013-09-28 13:54:37 +0300

    Added feature: Adding to LocalStorage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,27 @@
 /* Controllers */
 
 angular.module('Galery.controllers', [])
-  .controller('GaleryCtrl', ['$scope', 'Photos', 'Finder',
-                    function( $scope,   Photos,   Finder ) {
+  .controller('GaleryCtrl', ['$scope', 'Photos', 'Finder', 'Storage',
+                    function( $scope,   Photos,   Finder,  Storage ) {
+
+    var desctop = $scope.desctop = Storage.get()
+
+    if (desctop.length == 1) {
+      var ids = 0;
+      var parent_id = 0;
+      // var parent_id = 0;
+      localStorage.setItem('parent_id', 0);
+    }
 
     $scope.photos = Photos.getPicture('girls', 1);
     $scope.current_page = 1;
     $scope.isDisabled = false;
+
+    function splitation(name){
+      return name.split('/').slice(-1)[0].split('.')[0].slice(0, -2);
+    }
+
+
 
     $scope.searchPhotos = function(){
       $scope.ajax_loading = true;
@@ -38,9 +53,45 @@ angular.module('Galery.controllers', [])
     }
 
     $scope.addToFolder = function(photo){
+      ids += 1;
+
+      desctop.push({
+        id:     ids,
+        type:   'file',
+        parent: parent_id,
+        name:   splitation(photo),
+        src:    photo
+      });
+      Storage.put(desctop);
+
+
+  // factory.active = LocalStorage.get(active) || 0  // into LocalStorage
+  // factory.active = id
+
+
+
+
+
+      // var root = [ { type: 'folder', id: 1, parent: null, name: 'HOME' } ];
+      // localStorage.setItem("galery", JSON.stringify(root) );
+
+
+      // localStorage.getItem("root")
+
       // console.log(photo);
       // localStorage.setItem("file", photo);
       // console.log(localStorage.getItem("todoList"));
+
+
+
+
+    // desctop.push({
+    //   title: newTodo,
+    //   completed: false
+    // });
+    // Storage.put(desctop);
+
+
 
       // [
       //  { type: 'folder', id: 1, parent: null, name: 'HOME' },
--- app/js/services.js
+++ app/js/services.js
@@ -6,49 +6,61 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('Galery.services', ['ngResource'])
-  .config(function($httpProvider){
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  })
-  .factory('Photos', function($resource, $rootScope) {
-    $rootScope.loader = false;
-    var params = {
-      api_key: '51d0037db1691ef6163859f7f265e0ae',
-      method: 'flickr.photos.search',
-      format: 'json',
-      nojsoncallback: 1,
-      per_page: 4,
-    };
+.config(function($httpProvider){
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+.factory('Photos', function($resource, $rootScope) {
+  $rootScope.loader = false;
+  var params = {
+    api_key: '51d0037db1691ef6163859f7f265e0ae',
+    method: 'flickr.photos.search',
+    format: 'json',
+    nojsoncallback: 1,
+    per_page: 4,
+  };
 
-    var Picture = $resource("http://api.flickr.com/services/rest/", params);
+  var Picture = $resource("http://api.flickr.com/services/rest/", params);
 
-    function getPicture(query, page){
-      query = query || "girls";
-      page  = page  || 1;
+  function getPicture(query, page){
+    query = query || "girls";
+    page  = page  || 1;
 
-      return Picture.get({ text: query, page: page })
-        .$promise
-        .then(function (data) {
-          $rootScope.loader = true;
-          console.log('!!!');
-          return data.photos.photo
-            .map(function (item) {
-              return 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg';
-            });
-        });
-    }
+    return Picture.get({ text: query, page: page })
+    .$promise
+    .then(function (data) {
+      $rootScope.loader = true;
+       // console.log('!!!');
+      return data.photos.photo
+      .map(function (item) {
+        return 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg';
+      });
+    });
+  }
 
-    return {
-      getPicture: getPicture
-    };
-  })
-  .factory('Finder', function() {
-    var factory = {};
+  return {
+    getPicture: getPicture
+  };
+})
+.factory('Finder', function() {
+  var factory = {};
     // factory.active = LocalStorage.get(active) || 0  // into LocalStorage
-    factory.add = function(file){
-      console.log(file);
-    };
-    factory.setActive = function(id){
-      factory.active = id
+  factory.add = function(file){
+    console.log(file);
+  };
+  factory.setActive = function(id){
+    factory.active = id
+  }
+  return factory;
+})
+.factory('Storage', function () {
+  var STORAGE_ID = 'angular-js-storage';
+
+  return {
+    get: function () {
+      return JSON.parse(localStorage.getItem(STORAGE_ID) || "[" + JSON.stringify({ id: 1, type: 'folder', parent: null, name: 'HOME' }) + "]");
+    },
+    put: function (todos) {
+      localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
     }
-    return factory;
-  });
+  };
+});
